766ff84ae24bcc502b0667462bd2f458c8b952e7
Matt Keane <mattpkeane@gmail.com>
2020-05-10 18:01:07 -0500

    defined database and added models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
+
+class Recipe(models.Model):
+	url = models.CharField(max_length=2048),
+	title = models.CharField(max_length=256),
+	ingredients = ArrayField(
+		models.CharField(max_length=256))
+	instructions = ArrayField(
+		models.CharField(max_length=256))
+
+class Search(models.Model):
+	search_term = models.CharField(max_length=128)
+	recipe = models.ForeignKey("Recipe", on_delete=models.CASCADE)
 
 # Create your models here.
--- chef_hopper/settings.py
+++ chef_hopper/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    "api.apps.ApiConfig",
 ]
 
 MIDDLEWARE = [
@@ -76,8 +77,10 @@ WSGI_APPLICATION = 'chef_hopper.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': "chefhopper",
+        "HOST": "127.0.0.1",
+        "PORT": "5432"
     }
 }
 
