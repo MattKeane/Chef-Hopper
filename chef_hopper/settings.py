b281dfca03954cd7d0dc33db70c70cf9cc3ec913
Matt Keane <mattpkeane@gmail.com>
2020-05-12 14:07:37 -0500

    added CORS authentication

--- chef_hopper/settings.py
+++ chef_hopper/settings.py
@@ -40,9 +40,11 @@ INSTALLED_APPS = [
     'rest_framework',
     "api.apps.ApiConfig",
     "scraper.apps.ScraperConfig",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -123,3 +125,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# configuring CORS
+
+CORS_ORIGIN_WHITELIST = [
+    "http://localhost:3000"
+]
